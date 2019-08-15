9869b06b3dc81e62eb7fc4d849a23df010bc59be
Tamas <tamas@giffgaff.co.uk>
2019-08-15 15:12:46 +0100

    added assets

--- src/queries/withProduct/index.js
+++ src/queries/withProduct/index.js
@@ -1,12 +1,10 @@
 import { lifecycle } from 'recompose';
 
-
 const url = `${process.env.BASE_API_URL}products/`;
 
 const withProduct = lifecycle({
   state: { loading: true },
   componentDidMount() {
-    console.log(process.env.BASE_API_URL);
     fetch(`${url}${this.props.productId}`)
       .then(response => response.json())
       .then(data => this.setState({ loading: false, ...data }));
